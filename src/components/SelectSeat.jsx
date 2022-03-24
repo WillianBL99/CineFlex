62db9ea1cfacc46b6d928a83e6d365c1f5e0d63d
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-24 15:13:57 -0300

    Iplementarár seleção e deseleção de acentos

--- src/components/SelectSeat.jsx
+++ src/components/SelectSeat.jsx
@@ -8,23 +8,19 @@ export default function SelectSeat() {
 
     const { idSection } = useParams()
     const [dataSection, setDataSection] = useState({ seats: [] })
+    const [seatsSelected, setSeatsSelected] = useState([])
 
     useEffect(() => {
-        api.get(`/showtimes/2/seats`)
+        api.get(`/showtimes/${idSection}/seats`)
             .then(answer => setDataSection(answer.data))
-            .catch(error => console.log(error))
+            .catch(error => console.error(error))
     }, [])
 
     return (
         <main className="select-seat">
             <h2>Selecione o(s) assento(s)</h2>
             <div className="seats">
-                {console.log(dataSection)}
-                {dataSection.seats.map(seat =>
-                    <div key={seat.id} className={`seat ${seat.isAvailable?'':' unavailable'}`}>
-                        {seat.name}
-                    </div>
-                )}             
+                {dataSection.seats.map(seat => <Seat seat={seat} selecteds={seatsSelected} setSelecteds={setSeatsSelected} />)}
             </div>
 
             <div className="identifiers">
@@ -32,7 +28,7 @@ export default function SelectSeat() {
                 <div><div className="available"></div><p>Disponível</p></div>
                 <div><div className="unavailable"></div><p>Indisponível</p></div>
             </div>
-            
+
             <sectio className="user-data">
                 <p>Nome do comprador:</p>
                 <input type="text" placeholder="Digite seu nome..." />
@@ -40,7 +36,37 @@ export default function SelectSeat() {
                 <input type="number" placeholder="Digite seu CPF..." />
             </sectio>
 
-            <button>Reservar assento(s)</button>
+            <Link to={'/sucesso'} ><button>Reservar assento(s)</button></Link>
         </main>
+    )
+}
+
+function Seat({ seat: { id, name, isAvailable }, selecteds, setSelecteds }) {
+    const [selected, setSelected] = useState(false)
+
+    function verifySelected() {
+
+        if (isAvailable) {
+            if (!selected) setSelecteds([...selecteds, id])
+
+            else {
+                const index = selecteds.indexOf(id)
+
+                setSelecteds([...selecteds.slice(0, index),...selecteds.splice(index + 1)])
+            }
+
+            setSelected(!selected)
+        }
+
+    }
+
+    return (
+        <div
+            key={id}
+            onClick={verifySelected}
+            className={`seat ${!isAvailable ? 'unavailable' : selected ? 'selected' : ''}`}
+        >
+            {name}
+        </div>
     )
 }
