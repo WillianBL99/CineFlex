4d7347f21a14cb6fe8002017e9f637a74b50bf73
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-26 00:43:35 -0300

    Aperfeisoará responsividade do component Movies assim como a @meida geral

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,6 +2,7 @@ import '../assets/css/movies.css'
 
 import { useState, useEffect } from "react"
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import api from "../services/api"
 
 
@@ -16,23 +17,63 @@ export default function Movies() {
     }, [])
 
     return (
-        <main className="movies">
+        <Main>
             <h2>Selecione o filme</h2>
             <section>
-                {movies.map(({id,posterURL,title}) =>
+                {movies.map(({ id, posterURL, title }) =>
                     <Link to={`/filme/${id}`}>
-                        <Movie key={id+posterURL} img={posterURL} alt={title} />
+                        <Movie key={id + posterURL} img={posterURL} alt={title} />
                     </Link>)
                 }
             </section>
-        </main>
+        </Main>
     )
 }
 
 function Movie({ img, alt }) {
     return (
-        <article>
+        <Article>
             <img src={img} alt={alt} />
-        </article>
+        </Article>
     )
 }
+
+
+const Main = styled.main`
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding-inline: var(--padding-inline-main);
+
+    overflow-y: auto;
+
+    h2 {
+        padding: 2rem 0;
+
+        font-size: var(--font-subtitle);
+        color: var(--color-fonte);
+    }
+    section {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }   
+`
+const  Article = styled.article`
+    padding: 10px;
+    margin: 5px;
+
+    background: #FFFFFF;
+    box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+
+    img {
+        object-fit: cover;
+        object-position: center;
+        
+        width: calc(calc(100vw * 0.28) * var(--prop-img));
+        height: calc(calc(100vw * 0.45) * var(--prop-img));
+    }
+`
